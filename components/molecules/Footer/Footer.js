2d9f407206b327d0ef2fea2f55f077966347a0be
tsaniahm <www.tsaniahmunfidah@gmail.com>
2022-12-01 12:13:12 +0700

    add vignere

--- components/molecules/Footer/Footer.js
+++ components/molecules/Footer/Footer.js
@@ -4,7 +4,7 @@ import Heading from "../../atoms/text/Heading"
 const Footer = () => {
     return(
         <div className='relative min-h-full'>
-            <div className="absolute bottom-0 flex flex-col items-center justify-center w-full h-fit y-2 h h-18 bg-gradient-to-r from-indigo-800 to-black">
+            <div className="absolute bottom-0 flex flex-col items-center justify-center w-full y-2 h h-18 bg-gradient-to-r from-indigo-800 to-black">
             <div className="flex items-center justify-center w-full h-full">
                 <div className="w-8 h-8 mx-1 mt-3 mb-1">
                 <Image src="/Images/github.png" alt="copy img" width="28" height="28" className="mx-2" />
